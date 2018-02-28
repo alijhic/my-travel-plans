b37e9ab4d1e760ea1ba4fd62734d3eea15bfbaaa
alijhic <36937810+alijhic@users.noreply.github.com>
2018-02-28 23:48:58 +0000

    Add files via upload

---
+++ translate.py
@@ -0,0 +1,4 @@
+from googletrans import Translator
+translation=Translator()
+a=translation.translate("i want to sleep ",src='en',dest='so                                                                                                j')
+print a.text
